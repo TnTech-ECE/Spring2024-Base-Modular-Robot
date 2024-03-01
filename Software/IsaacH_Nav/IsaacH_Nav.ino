4ce4a925b15adbe5fac5956a6a545ea2ca3d9e6a
ibhoese42 <99824740+ibhoese42@users.noreply.github.com>
2024-02-29 18:27:42 -0600

    Update IsaacH_Nav.ino

--- Software/IsaacH_Nav/IsaacH_Nav.ino
+++ Software/IsaacH_Nav/IsaacH_Nav.ino
@@ -3,12 +3,14 @@ String start = "<";
 String end = ">";
 String c = ",";
 
-
+uint32_t currTime = 0;
+uint32_t prevTime = 0;
 void setup() {
   Serial.begin(9600);
   while (!Serial) yield();
 
   //setup_OLED();
+  setup_ObjectDetection();
   setup_LineFollowing();
   setup_Orientation();
 }
@@ -16,17 +18,11 @@ void setup() {
 void loop(){
   run_LineFollowing();
   run_Orientation();
-  //Serial.println(start + packet.lineFollowing + c + packet.heading + c + packet.pitch + c + packet.roll + c +packet.qw + c + packet.qx + c + packet.qy + c + packet.qz + end);
-  Serial.println(start + packet.lineFollowing + c + packet.heading + c + packet.pitch + end);
-  delay(10);
-}
-/*
-*Function Name: setup_OLED
-*Parameters: None
-*Returns: Void
-*Description: Initializes the OLED display
-*/
-void setup_OLED(){
-  oled.init();             
-  oled.clear();
+  run_Location();
+  run_ObjectDetection();
+  currTime = millis();
+  if((currTime-prevTime) > 40){
+    prevTime = currTime;
+    Serial.println(start + packet.lineFollowing + c + packet.heading + c + packet.roll + c + packet.pitch + c + packet.qw + c + packet.qx + c + packet.qy + c + packet.qz + c + packet.locationFront + c + packet.locationRight + c + packet.locationBack + c + packet.locationLeft + c + packet.objectDetectionSensors[0] + c + packet.objectDetectionSensors[1] + c + packet.objectDetectionSensors[2] + c + packet.objectDetectionSensors[3] + c + packet.objectDetectionSensors[4] + c + packet.objectDetectionSensors[5] + c + packet.objectDetectionSensors[6] + c + packet.objectDetectionSensors[7]  + end);
+  }
 }
